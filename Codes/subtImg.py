fa6e9fe8a6e3ba860d24f5f3ad9cf08f84b83dc2
youonlytrackonce <f_emre87@hotmail.com>
2021-10-07 18:29:35 +0300

    Bg and Fg subtraction

--- Codes/subtImg.py
+++ Codes/subtImg.py
@@ -4,6 +4,7 @@ import math
 from PIL import Image, ImageChops
 from numpy import asarray
 import os
+import copy
 
 
 
@@ -15,93 +16,129 @@ mask = cv.subtract(imgBG,imgFG)
 
 root_path = "/home/fatih/phd/CenterNet_DeepPBM/set0/cam1/"
 
-imgBG = cv.imread(root_path+'bg/000001.jpg')
-imgFG = cv.imread(root_path+'fg/000001.jpg')
+files = os.listdir(root_path+'bg')
 
-mask = cv.subtract(imgBG,imgFG)
-mask2 = cv.absdiff(imgBG,imgFG)
-cv.imwrite(root_path+'mask/absdiff_000000.jpg', mask)
+for refImg in files:
 
-Conv_hsv_Gray = cv.cvtColor(mask, cv.COLOR_BGR2GRAY)
-cv.imwrite(root_path+'mask/absdiffgray_000000.jpg', Conv_hsv_Gray)
+    imgBG = cv.imread(root_path+'bg/' + refImg)
+    imgFG = cv.imread(root_path+'fg/' + refImg)
 
-gaussBlur = cv.GaussianBlur(Conv_hsv_Gray, (5, 5), 0)
-cv.imwrite(root_path+'mask/gaussblur_000000.jpg', gaussBlur)
+    imgFG = cv.GaussianBlur(imgFG, (7,7), 3)
 
-medianBlur = cv.medianBlur(Conv_hsv_Gray, 5,0)
-cv.imwrite(root_path+'mask/medianblur_000000.jpg', medianBlur)
+    # mask = cv.subtract(imgBG,imgFG)
+    diffImg = cv.absdiff(imgBG,imgFG)
+    #cv.imwrite(root_path+'mask/absdiff_000001.jpg', diffImg)
 
-averageblur = cv.blur(Conv_hsv_Gray, (5,5))
-cv.imwrite(root_path+'mask/averageblur_000000.jpg', averageblur)
+    Conv_hsv_Gray = cv.cvtColor(diffImg, cv.COLOR_BGR2GRAY)
+    cv.imwrite(root_path+'diff/'+refImg, Conv_hsv_Gray)
 
-(gT, gaussblurotsu) = cv.threshold(gaussBlur,0,255,cv.THRESH_BINARY + cv.THRESH_OTSU)
-cv.imwrite(root_path+'mask/gaussblurotsu_000000.jpg', gaussblurotsu)
+    blurred = cv.GaussianBlur(Conv_hsv_Gray, (7,7), 3)
+    #cv.imwrite(root_path+'mask/absdiffgrayblur_000001.jpg', blurred)
 
-(mT, medianblurotsu) = cv.threshold(medianBlur,0,255,cv.THRESH_BINARY + cv.THRESH_OTSU)
-cv.imwrite(root_path+'mask/medianblurotsu_000000.jpg', medianblurotsu)
+    #thresh = cv.adaptiveThreshold(blurred, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 21, 10)
+    #cv.imwrite(root_path+'mask/adaptive_000001.jpg', thresh)
 
-(mT, averageblurotsu) = cv.threshold(averageblur,0,255,cv.THRESH_BINARY + cv.THRESH_OTSU)
-cv.imwrite(root_path+'mask/averageblurotsu_000000.jpg', averageblurotsu)
+    #mask1 = copy.deepcopy(blurred)
+    mask2 = copy.deepcopy(blurred)
+    """
+    (gT, threshotsu) = cv.threshold(mask1,0,255,cv.THRESH_OTSU)
+    cv.imwrite(root_path+'mask/threshotsu_000001.jpg', threshotsu)
+    gaussBlurOtsu = cv.GaussianBlur(threshotsu, (7, 7), 0)
+    cv.imwrite(root_path+'mask/gaussblurOtsu_000001.jpg', gaussBlurOtsu)
+    """
 
+    (gT, threshtri) = cv.threshold(mask2,0,255,cv.THRESH_TRIANGLE)
+    #cv.imwrite(root_path+'mask/threshtri_000001.jpg', threshtri)
+    gaussBlurTri = cv.GaussianBlur(threshtri, (7, 7), 0)
+    #cv.imwrite(root_path+'mask/' + refImg, gaussBlurTri)
 
+    normMask = gaussBlurTri/255
 
+    res = cv.bitwise_and(imgFG,imgFG,mask = gaussBlurTri)
+    #cv.imwrite(root_path+'masked/' + refImg, res)
+    """
+    mask2 = copy.deepcopy(Conv_hsv_Gray)
+    mask3 = copy.deepcopy(Conv_hsv_Gray)
 
-"""
-cv.imwrite(root_path+'mask/otsu_000000.jpg', abs_mask)
-cv.imwrite(root_path+'mask/blur_000000.jpg', thresholdwithblur)
-cv.imwrite(root_path+'mask/mask2_000000.jpg', mask2)
-cv.imwrite(root_path+'mask/imgBG_000000.jpg', imgBG)
-cv.imwrite(root_path+'mask/imgFG_000000.jpg', imgFG)
-##############################################
+    ###########################################################
+    #gaussBlur = cv.GaussianBlur(mask1, (5, 5), 0)
+    #cv.imwrite(root_path+'mask/gaussblur_000001.jpg', gaussBlur)
 
-mask2[abs_mask != 255] = [0,0,0]
-imgBG[abs_mask != 255] = [0,0,0]
-imgFG[abs_mask != 255] = [0,0,0] 
-"""
+    (gT, gaussblurotsu) = cv.threshold(mask1,0,255,cv.THRESH_BINARY + cv.THRESH_OTSU)
+    cv.imwrite(root_path+'mask/gaussblurotsu_000001.jpg', gaussblurotsu)
+    ###########################################################
+    #medianBlur = cv.medianBlur(mask2, 5,0)
+    #cv.imwrite(root_path+'mask/medianblur_000001.jpg', medianBlur)
 
+    (mT, medianblurotsu) = cv.threshold(mask2,0,255,cv.THRESH_BINARY + cv.THRESH_OTSU)
+    cv.imwrite(root_path+'mask/medianblurotsu_000001.jpg', medianblurotsu)
 
-# mask3 = imgFG - imgBG
-#gray = cv.cvtColor(cv.cvtColor(mask, cv.COLOR_BGR2RGB), cv.COLOR_BGR2GRAY)
+    ###########################################################
+    averageblur = cv.blur(mask3, (5,5))
+    cv.imwrite(root_path+'mask/averageblur_000001.jpg', averageblur)
 
-# cv.imwrite(root_path+'mask/negate_color_000000.jpg', mask3)
-
-# gray = cv.cvtColor(mask, cv.COLOR_BGR2GRAY)
-# cv.imwrite(root_path+'mask/subt_000000.jpg', gray)
-# cv.imwrite(root_path+'mask/mask_000000.jpg', mask)
-
-
-"""
-(T, threshold) = cv.threshold(gray,0,255,cv.THRESH_BINARY | cv.THRESH_OTSU)
-thresholdwithblur = cv.medianBlur(threshold, 15,0)
-cv.imwrite(root_path+'mask/binary_otsu_000000.jpg', thresholdwithblur)
-
-masked = cv.bitwise_and(imgFG, imgFG, mask=thresholdwithblur)
-maskwithblur = cv.medianBlur(masked, 15,0)
-cv.imwrite(root_path+'mask/masked_000000.jpg', maskwithblur)
-
-#mask = ImageChops.subtract(imgFG, imgBG)
-
-maskGray = skimage.color.rgb2gray(maskOrg)
-blur = skimage.filters.gaussian(maskGray, sigma=2)
-mask = blur < 0.8
-
-viewer = skimage.viewer.ImageViewer(mask)
-viewer.view()
+    (mT, averageblurotsu) = cv.threshold(averageblur,0,255,cv.THRESH_BINARY + cv.THRESH_OTSU)
+    cv.imwrite(root_path+'mask/averageblurotsu_000001.jpg', averageblurotsu)
+    """
 
 
-mask = mask.convert('L')
-maskArr = asarray(mask)
-print(maskArr)
-maskArr = maskArr > 127
-print(maskArr)
-mask = Image.fromarray(mask)
-mask = mask.convert('RGB')
-"""
-# th = 0.25*math.exp(1)
 
-# mask = mask > th
+    """
+    cv.imwrite(root_path+'mask/otsu_000000.jpg', abs_mask)
+    cv.imwrite(root_path+'mask/blur_000000.jpg', thresholdwithblur)
+    cv.imwrite(root_path+'mask/mask2_000000.jpg', mask2)
+    cv.imwrite(root_path+'mask/imgBG_000000.jpg', imgBG)
+    cv.imwrite(root_path+'mask/imgFG_000000.jpg', imgFG)
+    ##############################################
 
-# mask.save('/home/fatih/fatih_phd/DeepPBM/Codes/Result/BMC2012/Video_002/mask/mask3.jpg')
+    mask2[abs_mask != 255] = [0,0,0]
+    imgBG[abs_mask != 255] = [0,0,0]
+    imgFG[abs_mask != 255] = [0,0,0] 
+    """
 
-#cv.imwrite('/home/fatih/fatih_phd/DeepPBM/Codes/Result/BMC2012/Video_002/mask/mask1.jpg', mask)
+
+    # mask3 = imgFG - imgBG
+    #gray = cv.cvtColor(cv.cvtColor(mask, cv.COLOR_BGR2RGB), cv.COLOR_BGR2GRAY)
+
+    # cv.imwrite(root_path+'mask/negate_color_000000.jpg', mask3)
+
+    # gray = cv.cvtColor(mask, cv.COLOR_BGR2GRAY)
+    # cv.imwrite(root_path+'mask/subt_000000.jpg', gray)
+    # cv.imwrite(root_path+'mask/mask_000000.jpg', mask)
+
+
+    """
+    (T, threshold) = cv.threshold(gray,0,255,cv.THRESH_BINARY | cv.THRESH_OTSU)
+    thresholdwithblur = cv.medianBlur(threshold, 15,0)
+    cv.imwrite(root_path+'mask/binary_otsu_000000.jpg', thresholdwithblur)
+
+    masked = cv.bitwise_and(imgFG, imgFG, mask=thresholdwithblur)
+    maskwithblur = cv.medianBlur(masked, 15,0)
+    cv.imwrite(root_path+'mask/masked_000000.jpg', maskwithblur)
+
+    #mask = ImageChops.subtract(imgFG, imgBG)
+
+    maskGray = skimage.color.rgb2gray(maskOrg)
+    blur = skimage.filters.gaussian(maskGray, sigma=2)
+    mask = blur < 0.8
+
+    viewer = skimage.viewer.ImageViewer(mask)
+    viewer.view()
+
+
+    mask = mask.convert('L')
+    maskArr = asarray(mask)
+    print(maskArr)
+    maskArr = maskArr > 127
+    print(maskArr)
+    mask = Image.fromarray(mask)
+    mask = mask.convert('RGB')
+    """
+    # th = 0.25*math.exp(1)
+
+    # mask = mask > th
+
+    # mask.save('/home/fatih/fatih_phd/DeepPBM/Codes/Result/BMC2012/Video_002/mask/mask3.jpg')
+
+    #cv.imwrite('/home/fatih/fatih_phd/DeepPBM/Codes/Result/BMC2012/Video_002/mask/mask1.jpg', mask)
 
